c306c4e008c269b3732743e4397242769d272eee
programomcreatepink <87429860+programomcreatepink@users.noreply.github.com>
2021-09-10 20:23:29 +0530

    up

---
+++ application/logs/log-2021-09-10.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-09-10 10:46:45 --> 404 Page Not Found: Uploads/admin
+ERROR - 2021-09-10 10:46:45 --> 404 Page Not Found: Assets/images
+ERROR - 2021-09-10 13:05:23 --> 404 Page Not Found: Assets/images
+ERROR - 2021-09-10 13:12:14 --> 404 Page Not Found: Uploads/admin
